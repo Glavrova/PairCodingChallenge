8625072ffce455a3cc72e99cd554a9365764df85
Galina <glavrova@gmail.com>
2023-11-14 17:53:38 +0200

    Removed comments

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        int[] nums = {1,2,3,2,2,1};
+
         Map<Integer, Integer> itemCount = new HashMap<>();
         Map.Entry<Integer,Integer> max = null;
 
-//        int[] itemList = {1,2,3,2,2,1};
-//        for (int i : itemList) {
-//            itemCount.merge(Integer.valueOf(i), 1, Integer::sum);
-//        }
-//        for (Map.Entry<Integer, Integer> e : itemCount.entrySet()) {
-//            if (max == null || (e.getValue() > max.getValue())) {
-//                max = e;
-//            }
-//        }
+        for (int i : nums) {
+            itemCount.merge(Integer.valueOf(i), 1, Integer::sum);
+        }
 
-        List<Integer> itemList = Arrays.asList(1, 2, 3, 2, 2, 1);
-        itemList.stream().forEach(i -> itemCount.merge(Integer.valueOf(i), 1, Integer::sum));
         max = itemCount.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get();
 
         System.out.println(String.format("The majority item is %d with %d occurrences", max.getKey(), max.getValue()));
